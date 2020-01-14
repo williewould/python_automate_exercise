916495434edd65a91845ea7c5763f77c9888de7d
williewould <tyzhaowu@gmail.com>
2020-01-14 23:17:10 +0800

    zip 压缩文件

--- ch9orgnzingFiles/fileOgnizing.py
+++ ch9orgnzingFiles/fileOgnizing.py
@@ -39,7 +39,7 @@ baconFile.close()
 send2trash.send2trash('bacon.txt')
 
 # 遍历文件树 os.walk() 函数
- for folderName, subfolders, filenames in os.walk('C:\\Users\\zhaowu\\spam'):
+for folderName, subfolders, filenames in os.walk('C:\\Users\\zhaowu\\spam'):
     print('The current folder is '+folderName)
     for subfolder in subfolders:
         print('SUBFOLDER OF ' + folderName + ':' + subfolder)
@@ -47,4 +47,33 @@ send2trash.send2trash('bacon.txt')
         print('FILE INSIDE ' + folderName + ':' + filename)
     print('')
 
-# TODO 压缩文件
+#%% 压缩文件
+import zipfile,os
+from pathlib import Path
+p = Path.home()
+# 得到一个zip的对象
+exampleZip = zipfile.ZipFile(p/'spam.zip')
+# 得到zip对象的信息参数，目录信息 需要与list中的格式一致
+spamInfo = exampleZip.getinfo('spam/spam.txt')
+spamInfo.file_size
+# 关闭zip对象
+exampleZip.close()
+# 显示压缩率 file_size是文件原始大小 compress_size是压缩后的大小
+exampleZip = zipfile.ZipFile(p/'spam2.zip')
+spamInfo = exampleZip.getinfo('spam/spam.txt')
+spamInfo.file_size
+f'Compressed file is {round(spamInfo.file_size / spamInfo.compress_size,2)} x smaller'
+
+# 解压缩文件
+exampleZip = zipfile.ZipFile(p/'spam2.zip')
+exampleZip.extractall(p)    #p是文件目录 C:\Users\zw
+exampleZip.close()
+
+# 新建压缩文件 压缩内容精确到后缀，否则知识压缩文件夹，却不包含里面的文件
+newZip = zipfile.ZipFile(p/'new.zip','w')
+newZip.write(p/'eggs.txt',compress_type=zipfile.ZIP_DEFLATED)
+newZip.close()
+# 添加新文件到压缩文件
+newZip = zipfile.ZipFile(p/'new.zip','a')
+newZip.write('eggs\\bb.txt',compress_type=zipfile.ZIP_DEFLATED)
+newZip.close()
