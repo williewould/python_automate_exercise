daeef18b7a82d5cb2ec4bbe3db859c781c126e86
williewould <tyzhaowu@gmail.com>
2020-01-15 14:32:24 +0800

    debug

---
+++ ch10debugging/debugging.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2020/01/15
+@Version:    1.0
+@author :    zhaowu
+@Ref    :    book
+'''
+#%% try except; raise exception 来处理错误
+# raise需要在try except中调用
+raise Exception('This is the error message')
