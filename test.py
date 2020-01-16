15c8fded23ce0c9974ec481387dacbfcc7543578
williewould <tyzhaowu@gmail.com>
2020-01-16 17:14:16 +0800

    new test to learn vs code

--- test.py
+++ test.py
@@ -5,5 +5,16 @@ Created on 2020/01/06
 @author :    zhaowu
 @Ref    :    book
 '''
-print("hello word")
+import numpy as np
+import matplotlib.pyplot as plt
+
 # braunch 功能测试，在github上操作
+msg = 'hello,word'
+print("hello word\n")
+print(msg)
+
+
+# Create a list of evenly-spaced numbers over the range
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
