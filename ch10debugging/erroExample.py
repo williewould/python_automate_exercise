8fa96707b4e9469ef34c3a43d57c38435ef9f6bf
williewould <tyzhaowu@gmail.com>
2020-01-16 10:03:18 +0800

    flake 8 代码格式化

--- ch10debugging/erroExample.py
+++ ch10debugging/erroExample.py
@@ -5,19 +5,25 @@ Created on Wed Jan 15 16:28:30 2020
 @author: zhaowu
 """
 
+
+import traceback
+
+
 def spam():
     bacon()
-    
+
+
 def bacon():
     raise Exception('This is the error message.')
 
+
 spam()
 
-#%%报错信息打印， 生成日志信息
+# %%报错信息打印， 生成日志信息
 try:
     raise Exception('This is the error messgage.')
 except:
-    errorFile = open('errorInfo.txt','w')
+    errorFile = open('errorInfo.txt', 'w')
     errorFile.write(traceback.format_exc())
     errorFile.close()
     print('The traceback info was written to errorInfo.txt.')
