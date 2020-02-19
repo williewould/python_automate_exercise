3a170ace95746b5e3fb36847c8aa9df313e0a7d3
williewoud <tyzhaowu@gmail.com>
2020-02-19 23:05:08 +0800

    Flake8

--- ch15wordAndPDF/pdfFile.py
+++ ch15wordAndPDF/pdfFile.py
@@ -10,14 +10,14 @@ Created on 2020/02/19
 # %% 打开PDF文档
 import PyPDF2
 
-pdfFileObj = open('meetingminutes.pdf','rb')
+pdfFileObj = open('meetingminutes.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pdfReader.numPages
 pageObj = pdfReader.getPage(0)
 print(pageObj.extractText())
 pdfFileObj.close()
 # %% 有密码的情况下，打开机密的PDF文档
-pdfReader2 = PyPDF2.PdfFileReader(open('encrypted.pdf','rb'))
+pdfReader2 = PyPDF2.PdfFileReader(open('encrypted.pdf', 'rb'))
 pdfReader2.isEncrypted
 print(pdfReader2.getPage(0))
 pdfReader2.decrypt('rosebud')
